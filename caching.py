ae809c3dad4fe5d5f1172b6c8b7e0a84e905773d
bushig <azerot966@gmail.com>
2017-03-18 06:34:36 +0300

    Added prefix to webm data in cache

--- caching.py
+++ caching.py
@@ -11,20 +11,20 @@ def set_cache(webm_data):
     """
     md5 = webm_data.get('md5', None)  # TODO: check its schema
     if md5:
-        r_type = r.type(md5)
+        r_type = r.type('webm:' + md5)
         if r_type == 'none':
-            r.hmset(md5, webm_data)
+            r.hmset('webm:' + md5, webm_data)
             r.rpush('webmlist', md5)
             return True
     return False
 
 
 def set_cache_delayed(md5):
-    r.set(md5, 'delayed')
+    r.set('webm:' + md5, 'delayed')
 
 
 def del_cache(md5):
-    r.delete(md5)
+    r.delete('webm:' + md5)
 
 
 def pop_webm_from_redis_list():
@@ -32,7 +32,7 @@ def pop_webm_from_redis_list():
 
 
 def save_webm_to_db(md5):
-    data = r.hgetall(md5)
+    data = r.hgetall('webm:' + md5)
     session = Session()
     webm = session.query(WEBM).get(md5)
     webm.views = data['views']
@@ -46,10 +46,10 @@ def get_cache(md5):
     """
     :return: WEBM data, 'delayed' message or None depending on results
     """
-    r_type = r.type(md5)
+    r_type = r.type('webm:' + md5)
 
     if r_type == 'hash':
-        cache = r.hgetall(md5)
+        cache = r.hgetall('webm:' + md5)
         if cache.get('screamer_chance', None) == 'None':  # Because of redis-py (nil) value casting
             cache['screamer_chance'] = None
         return cache
@@ -63,33 +63,33 @@ def incr_views(md5):
     """
     :return: True if increased, False if failed
     """
-    r_type = r.type(md5)
+    r_type = r.type('webm:' + md5)
     print(md5, r_type)  # TODO: Если нет в кэше, загрузить и увеличить счетчик.
     if r_type == 'hash':
-        r.hincrby(md5, 'views')
+        r.hincrby('webm:' + md5, 'views')
         return True
     return False
 
 def like_webm(md5, ip, action):
     # Если все прошло удачно - вернуть словарь с количеством лайков/дизлайков, иначе вернуть None
     # в ip:127.0.0.1 хранится хэш вида {viewed:{ISO TIME} action:{nil, like or dislike}}
-    r_type = r.type(md5)
+    r_type = r.type('webm:' + md5)
     if r_type == 'hash':
         ip_action = r.hget('ip:' + ip + ":" + md5, 'action')
         # Убираем лайк
         if action == ip_action:
-            r.hincrby(md5, action + 's', -1)
+            r.hincrby('webm:' + md5, action + 's', -1)
             r.hset('ip:' + ip + ":" + md5, 'action', None)
             action = None
         # Спокойно ставим лайк
         elif ip_action is None:
-            r.hincrby(md5, action + 's')
+            r.hincrby('webm:' + md5, action + 's')
             r.hset('ip:' + ip + ":" + md5, 'action', action)
         # Убираем одно действие и ставим другое
         else:
-            r.hincrby(md5, action + 's')
-            r.hincrby(md5, ip_action + 's', -1)
+            r.hincrby('webm:' + md5, action + 's')
+            r.hincrby('webm:' + md5, ip_action + 's', -1)
             r.hset('ip:' + ip + ":" + md5, 'action', action)
-        result = r.hmget(md5, ('likes', 'dislikes'))
+        result = r.hmget('webm:' + md5, ('likes', 'dislikes'))
         return {'md5': md5, 'likes': result[0], 'dislikes': result[1], 'action': action}
     return
